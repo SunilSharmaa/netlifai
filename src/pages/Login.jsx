68f591ab3c2f5e1ac614fde5ab397b4276c964f6
Sunil Sharma <sunilsharma.workit@gmail.com>
2025-03-03 10:31:46 +0530

    create login page with validation

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,12 +1,77 @@
+import { useRef, useState } from "react";
+import validateCredential from "../utils/validate";
+
 const Login = () => {
+  const [isSignIn, setIsSignIn] = useState(true);
+  const [errorMessage, setErrorMessage] = useState(null);
+  const email = useRef(null);
+  const password = useRef(null);
+  const userName = useRef(null);
+
+  const setSignInSignUp = () => {
+    setErrorMessage(null)
+    setIsSignIn(!isSignIn);
+  };
+
+  const onSubmitForm = (userName, email, password) => {
+    let error = validateCredential(userName?.current?.value, email?.current?.value, password?.current?.value);
+    setErrorMessage(error);
+  }
   return (
     <div className="relative">
-
-      <img src="https://assets.nflxext.com/ffe/siteui/vlv3/04ef06cc-5f81-4a8e-8db0-6430ba4af286/web/IN-en-20250224-TRIFECTA-perspective_3a9c67b5-1d1d-49be-8499-d179f6389935_large.jpg" 
-      alt="background image"
-      className="object-cover" />
+      <img
+        src="https://assets.nflxext.com/ffe/siteui/vlv3/04ef06cc-5f81-4a8e-8db0-6430ba4af286/web/IN-en-20250224-TRIFECTA-perspective_3a9c67b5-1d1d-49be-8499-d179f6389935_large.jpg"
+        alt="background image"
+        className="h-screen w-full"
+      />
 
       <div className="absolute inset-0 bg-black opacity-60"></div>
+
+      <form
+        className="absolute top-[25%] left-1/2 -translate-x-1/2 w-100 bg-black/70 px-12 py-12 rounded"
+        action=""
+        onSubmit={(e) => e.preventDefault()}
+      >
+        <h2 className="text-white font-bold text-2xl mb-8">
+          {isSignIn ? "Sign In" : "Sign Up"}
+        </h2>
+        {!isSignIn && (
+          <input
+            ref={userName}
+            className="text-white bg-gray-800 py-2 pl-4 pr-2 w-full rounded focus:outline-none"
+            type="text"
+            placeholder="Name"
+          />
+        )}
+        <input
+          ref={email}
+          className="text-white bg-gray-800 py-2 pl-4 pr-2 w-full rounded focus:outline-none mt-4"
+          type="text"
+          placeholder="Email"
+        />
+        <input
+          ref={password}
+          className="text-white mt-4 bg-gray-800 py-2 pl-4 pr-2 w-full rounded focus:outline-none"
+          type="password"
+          placeholder="Password"
+        />
+        {errorMessage !== null && (
+            <p className="text-red-600 mt-4">{errorMessage}</p>
+        )}
+        <button onClick={()=>onSubmitForm(userName, email, password)} className="text-white bg-red-600 w-full mt-14 p-2 rounded cursor-pointer">
+          {isSignIn ? "Sign In" : "Sign Up"}
+        </button>
+        <span className="text-white mt-6 inline-block">
+          {isSignIn ? "New to Netlifai?" : "Already user?"}{" "}
+        </span>
+        <span
+          className="text-white hover:underline cursor-pointer"
+          onClick={setSignInSignUp}
+        >
+          {" "}
+          {isSignIn ? "Sign up now" : "Sign In"}
+        </span>
+      </form>
     </div>
   );
 };
