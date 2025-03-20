594b9e29dd8d90801810ac3f1dd33987e348e742
Sunil Sharma <sunilsharma.workit@gmail.com>
2025-03-20 14:07:35 +0530

    upcoming movies hook moved to component folder

--- src/pages/Browse.jsx
+++ src/pages/Browse.jsx
@@ -3,7 +3,7 @@ import ExploreSection from "../components/ExploreSection";
 import useNowPlayingMovies from "../hooks/useNowPlayingMovies";
 import usePopularMovies from "../hooks/usePopularMovies";
 import useTopRatedMovies from "../hooks/useTopRatedMovies";
-import useUpComingMovies from "../hooks/useUpComingMovies";
+import useUpComingMovies from "../components/useUpComingMovies";
 
 
 const Browse = () => {
