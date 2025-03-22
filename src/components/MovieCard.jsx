ccf0e91806104a65eada1f0991264b78c0f19750
Sunil Sharma <sunilsharma.workit@gmail.com>
2025-03-22 12:11:58 +0530

    movieCard render correctly with proper width

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,12 +1,17 @@
 import { IMAGE_CDN_LINK } from "../utils/constant";
 
-const MovieCard = ({posterPath}) => {
-    if(!posterPath) return
+const MovieCard = ({ posterPath }) => {
+  if (!posterPath) return;
+  
   return (
     <div className="mr-3 w-40 ">
-        <img className="w-full min-h-60 object-cover overflow-hidden" src={IMAGE_CDN_LINK + posterPath} alt="" />
+      <img
+        className="w-full min-h-60 object-cover overflow-hidden"
+        src={IMAGE_CDN_LINK + posterPath}
+        alt=""
+      />
     </div>
-  )
-}
+  );
+};
 
-export default MovieCard
+export default MovieCard;
