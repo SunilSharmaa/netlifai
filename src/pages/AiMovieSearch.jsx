242dbcb2d8a54aed5f83d19d74a6579aa3b2ab26
Sunil Sharma <sunilsharma.workit@gmail.com>
2025-03-24 15:07:50 +0530

    add background image to AiMovieSearch page

--- src/pages/AiMovieSearch.jsx
+++ src/pages/AiMovieSearch.jsx
@@ -1,6 +1,13 @@
 const AiMovieSearch = () => {
   return (
-    <div className="text-4xl text-red-600">AiMovieSearch</div>
+    <div>
+        <img
+        src="https://assets.nflxext.com/ffe/siteui/vlv3/04ef06cc-5f81-4a8e-8db0-6430ba4af286/web/IN-en-20250224-TRIFECTA-perspective_3a9c67b5-1d1d-49be-8499-d179f6389935_large.jpg"
+        alt="background image"
+        className="h-screen w-full"
+      />
+      <div className="absolute inset-0 bg-black opacity-75"></div>
+    </div>
   )
 }
 
