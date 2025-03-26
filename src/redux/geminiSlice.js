5243f4ab19426b97116cb63f766b6d81d24f85f6
Sunil Sharma <sunilsharma.workit@gmail.com>
2025-03-26 10:42:43 +0530

    Update Redux state management for AI movie search

--- src/redux/geminiSlice.js
+++ src/redux/geminiSlice.js
@@ -4,13 +4,20 @@ const geminiSlice = createSlice({
     name : "gemini",
     initialState : {
         inputText : null,
+        movieName : null,
+        movieData : null,
     },
     reducers : {
         addInputText : (state, action)=> {
             state.inputText = action.payload;
+        },
+        addMovieNameAndData : (state, action)=> {
+            const {movieName, movieData} = action.payload;
+            state.movieName = movieName;
+            state.movieData = movieData;
         }
     }
 })
 
-export const {addInputText} = geminiSlice.actions;
+export const {addInputText, addMovieNameAndData} = geminiSlice.actions;
 export default geminiSlice.reducer;
