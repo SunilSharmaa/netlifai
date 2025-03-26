d1550b05a10cff6f5a8464f5314db4dd61f7c017
Sunil Sharma <sunilsharma.workit@gmail.com>
2025-03-26 15:54:34 +0530

    ui enhance for movie search page

--- src/components/MovieSearchBox.jsx
+++ src/components/MovieSearchBox.jsx
@@ -7,24 +7,31 @@ const MovieSearchBox = () => {
   const dispatch = useDispatch();
   const inputText = useRef();
 
-  const searchMovie = ()=> {
-    dispatch(addInputText(inputText.current.value))
-  }
+  const searchMovie = () => {
+    dispatch(addInputText(inputText.current.value));
+  };
 
   return (
-    <div className="text-white absolute top-[20%] left-[50%] -translate-x-[50%] w-[50%]">
-      <form 
-        className="space-x-6 flex justify-between py-2 px-6 rounded bg-black/60"
-        onSubmit={(e) => e.preventDefault()}
-      >
-        <input
-          ref={inputText}
-          type="text"
-          placeholder="search for movies"
-          className="px-4 py-2 rounded w-full focus:outline-none"
-        />
-        <button className="bg-red-700 py-1 px-6 rounded cursor-pointer" onClick={searchMovie}>search</button>
-      </form>
+    <div className="text-white absolute top-[20%] left-[50%] -translate-x-[50%] w-full">
+      <div className="w-[50%] mx-auto">
+        <form
+          className="space-x-6 flex justify-between py-2 px-6 rounded bg-black/60"
+          onSubmit={(e) => e.preventDefault()}
+        >
+          <input
+            ref={inputText}
+            type="text"
+            placeholder="search for movies"
+            className="px-4 py-2 rounded w-full focus:outline-none"
+          />
+          <button
+            className="bg-red-700 py-1 px-6 rounded cursor-pointer"
+            onClick={searchMovie}
+          >
+            search
+          </button>
+        </form>
+      </div>
       <SearchResults />
     </div>
   );
