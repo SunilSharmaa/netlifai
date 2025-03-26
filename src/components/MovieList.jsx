82998ff7630c2f24018e19e566369e90bc279bf1
Sunil Sharma <sunilsharma.workit@gmail.com>
2025-03-26 10:42:01 +0530

    Update UI components for movie search and results display

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -4,8 +4,8 @@ const MovieList = ({section, list}) => {
   if(!section || !list) return
   
   return (
-    <div className="mt-4 ml-12 mx-auto flex overflow-x-auto scrollbar-hide flex-col">
-      <h2 className="text-2xl font-semibold mb-2 text-white">{section}</h2>
+    <div className="mt-4 ml-12 mr-12 mx-auto flex overflow-x-auto scrollbar-hide flex-col">
+      <h2 className="text-2xl font-semibold mb-2 text-white mt-2">{section}</h2>
       <div className="flex">
       <div className=" flex">
           {list.map((movie)=> <MovieCard key={movie.id} posterPath={movie.poster_path}/>)}
