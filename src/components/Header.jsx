68a7d1c9f9f4d94cce98bbdf400e11cc5a81a898
Sunil Sharma <sunilsharma.workit@gmail.com>
2025-04-01 17:43:17 +0530

    fix in header responsiveness

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,7 +8,9 @@ import { toggleSearchPageActive } from "../redux/geminiSlice";
 const Header = () => {
   let dispatch = useDispatch();
   let user = useSelector((state) => state?.user);
-  let isSearchPageActive = useSelector((state)=> state?.gemini?.isSearchPageActive);
+  let isSearchPageActive = useSelector(
+    (state) => state?.gemini?.isSearchPageActive
+  );
 
   let navigate = useNavigate();
 
@@ -16,7 +18,6 @@ const Header = () => {
     dispatch(toggleSearchPageActive());
 
     isSearchPageActive ? navigate("/browse") : navigate("/aimoviesearch");
-
   };
 
   const navigateToHome = () => {
@@ -34,30 +35,34 @@ const Header = () => {
       });
   };
   return (
-    <div className="absolute w-full pl-16 pt-4 pb-8 bg-gradient-to-b from-black z-10 flex justify-between items-center">
-      <img
-        onClick={navigateToHome}
-        className="w-30"
-        src="https://help.nflxext.com/helpcenter/OneTrust/oneTrust_production/consent/87b6a5c0-0104-4e96-a291-092c11350111/01938dc4-59b3-7bbc-b635-c4131030e85f/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
-        alt="netflix logo"
-      />
+    <div className="absolute w-full  pt-4 pb-8 bg-gradient-to-b from-black z-10 ">
+      <div className="w-[90%] mx-auto flex justify-between items-center">
+        <img
+          onClick={navigateToHome}
+          className="w-30"
+          src="https://help.nflxext.com/helpcenter/OneTrust/oneTrust_production/consent/87b6a5c0-0104-4e96-a291-092c11350111/01938dc4-59b3-7bbc-b635-c4131030e85f/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
+          alt="netflix logo"
+        />
 
-      <div className="pr-6 space-x-4">
-        {user && <button
-          className="text-white bg-amber-600 px-4 py-1 rounded cursor-pointer"
-          onClick={navigateToPage}
-        >
-          { isSearchPageActive ? "Home Page" : "AI Movie Search"}
-        </button>}
-        
-        {user && (
-          <button
-            className="bg-red-700 px-4 py-1 text-white rounded cursor-pointer"
-            onClick={userSignOut}
-          >
-            Sign out
-          </button>
-        )}
+        <div className="space-x-4">
+          {user && (
+            <button
+              className="text-white text-sm bg-amber-600 px-3 py-1 rounded cursor-pointer"
+              onClick={navigateToPage}
+            >
+              {isSearchPageActive ? "Home Page" : "AI Movie Search"}
+            </button>
+          )}
+
+          {user && (
+            <button
+              className="text-sm  bg-red-700 px-3 py-1 text-white rounded cursor-pointer"
+              onClick={userSignOut}
+            >
+              Sign out
+            </button>
+          )}
+        </div>
       </div>
     </div>
   );
