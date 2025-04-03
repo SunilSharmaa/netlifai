b5ea3946d0999e34007e99c6d8aaf3a458f0f826
Sunil Sharma <sunilsharma.workit@gmail.com>
2025-04-03 11:47:23 +0530

    fix in UI

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -4,9 +4,9 @@ const MovieCard = ({ posterPath }) => {
   if (!posterPath) return;
   
   return (
-    <div className="w-26 ">
+    <div className="w-26 sm:w-40">
       <img
-        className="w-full min-h-40 max-h-40 mb-4 object-cover overflow-hidden rounded-lg shadow-md shadow-neutral-800"
+        className="w-full min-h-40 max-h-40 sm:min-h-60 sm:max-h-60 mb-4 object-cover overflow-hidden rounded-lg shadow-md shadow-neutral-800"
         src={IMAGE_CDN_LINK + posterPath}
         alt=""
       />
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -4,13 +4,13 @@ const MovieList = ({ section, list }) => {
   if (!section || !list) return;
 
   return (
-    <div className="w-[95%] mx-auto mt-2">
-        <h2 className=" font-semibold text-white mb-1">
+    <div className="w-[95%] mx-auto mt-2 sm:mt-4">
+        <h2 className=" font-semibold text-white mb-1 sm:text-xl">
           {section}
         </h2>
       <div className="mt- x-auto flex overflow-x-auto scrollbar-hide flex-col">
         <div className="flex">
-          <div className="flex space-x-1.5">
+          <div className="flex space-x-1.5 sm:space-x-3">
             {list.map((movie) => (
               <MovieCard key={movie.id} posterPath={movie.poster_path} />
             ))}
