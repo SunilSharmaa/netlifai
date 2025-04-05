0a2cf4c3f93d7333dae2a7a0368afed35ce12a54
Sunil Sharma <sunilsharma.workit@gmail.com>
2025-04-05 15:52:21 +0530

    ui fix in movie search page

--- src/components/MovieSearchBox.jsx
+++ src/components/MovieSearchBox.jsx
@@ -15,20 +15,20 @@ const MovieSearchBox = () => {
     <div className="text-white absolute top-[20%] left-[50%] -translate-x-[50%] w-full">
       <div className="w-[95%] sm:w-[50%] mx-auto">
         <form
-          className="space-x-6 flex justify-between py-1 sm:py-2 px-2  sm:px-6 rounded bg-black/60"
+          className="space-x-6 flex justify-between py-1 sm:py-2 px-2  sm:px-6 rounded bg-black/60 items-center"
           onSubmit={(e) => e.preventDefault()}
         >
           <input
             ref={inputText}
             type="text"
-            placeholder="search for movies"
+            placeholder="Search for movies"
             className="px-4 py-2 rounded w-full focus:outline-none"
           />
           <button
             className="bg-red-700 py-1 px-4 sm:px-6 rounded cursor-pointer"
             onClick={searchMovie}
           >
-            search
+            Search
           </button>
         </form>
       </div>
