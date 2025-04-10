9c863bef43fefc3bfe87c6287ea6f23502e56bf3
Sunil Sharma <sunilsharma.workit@gmail.com>
2025-04-10 20:43:38 +0530

    remove console.log

--- src/components/VideoTitle.jsx
+++ src/components/VideoTitle.jsx
@@ -4,15 +4,15 @@ import { IMAGE_CDN_LINK, MOVIE_IMAGES_URL, OPTIONS } from "../utils/constant";
 const VideoTitle = ({ movieId, overview, language }) => {
   let [logoPath, setLogoPath] = useState(null);
   // console.log(title);
-  console.log(language);
+  // console.log(language);
   useEffect(() => {
     const fetchLogo = async () => {
       const data = await fetch(MOVIE_IMAGES_URL(movieId),OPTIONS);
       const json = await data.json();
-      console.log(json);
+      // console.log(json);
       let filterData = json.logos.filter((data) => data.iso_639_1 === language);
       // filterData.map((data)=>console.log(data.file_path))
-      console.log(filterData[0].file_path);
+      // console.log(filterData[0].file_path);
       setLogoPath(IMAGE_CDN_LINK + filterData[0].file_path);
     };
 
