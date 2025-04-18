f20fa4fe68845991d481c5d01f69a23834289d6e
Sunil Sharma <sunilsharma.workit@gmail.com>
2025-04-18 20:03:25 +0530

    refactor: clean SearchResults and fix incorrect movieData access

--- src/components/SearchResults.jsx
+++ src/components/SearchResults.jsx
@@ -1,17 +1,19 @@
 import { useSelector } from "react-redux";
-import useSearchResults from "../hooks/useSearchResult"
+import useSearchResults from "../hooks/useSearchResult";
 import MovieList from "./MovieList";
 
 const SearchResults = () => {
-    useSearchResults();
-    const {movieName, movieData} = useSelector(store => store.gemini);
-    if(!movieName && !movieData) return
-    console.log(movieName, movieData);
+  useSearchResults();
+  const { movieName, movieData } = useSelector((store) => store.gemini);
+  if (!movieName && !movieData) return;
+  
   return (
     <div className="bg-black/70 pt-2 mt-2">
-        {movieName.map((val, index)=> <MovieList key={val} section={val} list={movieData[index].results}/>)}
+      {movieName.map((val, index) => (
+        <MovieList key={val} section={val} list={movieData[index]} />
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default SearchResults
+export default SearchResults;
