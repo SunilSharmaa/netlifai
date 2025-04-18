004879aa0ce3b54c7cfc4784fcd943e5ece687a1
Sunil Sharma <sunilsharma.workit@gmail.com>
2025-04-18 20:10:16 +0530

    refactor: clean up HeroSection component formatting and remove unused props
    - Removed unused `original_title` variable
    - Improved readability by formatting selector and props
    - Ensured only necessary movie data is passed to child components

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -3,17 +3,22 @@ import VideoTitle from "./VideoTitle";
 import { useSelector } from "react-redux";
 
 const HeroSection = () => {
+  const nowPlayingMovies = useSelector(
+    (store) => store.movies.nowPlayingMovies
+  );
 
-  const nowPlayingMovies = useSelector((store)=> store.movies.nowPlayingMovies);
-  if(!nowPlayingMovies) return;
-  
-  const {original_title, id, overview, original_language, poster_path} = nowPlayingMovies[0];
-  // console.log(poster_path);
-  
+  if (!nowPlayingMovies) return;
+
+  const { id, overview, original_language, poster_path } = nowPlayingMovies[0];
+
   return (
     <>
-      <BackgroundVideo movieId={id} poster={poster_path}/>
-      <VideoTitle movieId={id} overview={overview} language={original_language}/>
+      <BackgroundVideo movieId={id} poster={poster_path} />
+      <VideoTitle
+        movieId={id}
+        overview={overview}
+        language={original_language}
+      />
     </>
   );
 };
